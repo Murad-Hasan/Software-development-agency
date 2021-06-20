8216c775acf5b673e350130875a4e47b1b9e112f
Murad-Hasan <muradhasan.7845@gmail.com>
2021-06-21 01:30:56 +0600

    header & hero added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,31 +1,15 @@
-import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
-// import { useContext } from "react";
-// import { UserContext } from "../../../../App";
+import React from 'react';
+import './Header.css'
+import HeroSection from './HeroSection/HeroSection';
+import NavBar from './NavBar';
 
 const Header = () => {
-//   const [loggedInUser, setLoggedInUser] = useContext(UserContext)
-  return (
-    <Navbar sticky="top" collapseOnSelect expand="md" >
-      <div className="container">
-        <Navbar.Brand href="/home">
-          <h2 className='font-monospace border-1'>Softonic Solution</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link className='text-white' as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link className='text-white' as={Link} to="/aboutUs">About Us</Nav.Link>
-            <Nav.Link className='text-white' as={Link} to="/contact">Contact </Nav.Link>
-            <Nav.Link className='text-white' as={Link} to="/dashboard">Dashboard</Nav.Link>
-            {/* {  localStorage.getItem('user')   ? <button onClick={ () => setLoggedInUser(!localStorage.removeItem('user') )} className='btn btn-danger'>LogOut</button> : <Link to='/login'><button className='btn btn-danger'>Login</button></Link>
-            } */}
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-  );
+    return (
+        <div className='header'>
+            <NavBar/>
+            <HeroSection/>
+        </div>
+    );
 };
 
 export default Header;
